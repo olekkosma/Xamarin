44bc4dcdbbc621dfb1687ecf86a1add5309ba301
olekkosma <akosma@sigma.ug.edu.pl>
2018-03-27 13:35:47 +0200

    UWP screen size setted

--- GymProgress/GymProgress/GymProgress.Android/MainActivity.cs
+++ GymProgress/GymProgress/GymProgress.Android/MainActivity.cs
@@ -16,9 +16,6 @@ namespace GymProgress.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
-           
-
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
--- GymProgress/GymProgress/GymProgress.UWP/MainPage.xaml.cs
+++ GymProgress/GymProgress/GymProgress.UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Xamarin.Forms;
 
 namespace GymProgress.UWP
 {
@@ -20,7 +22,8 @@ namespace GymProgress.UWP
         public MainPage()
         {
             this.InitializeComponent();
-
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            
             LoadApplication(new GymProgress.App());
         }
     }
